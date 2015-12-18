7d75c1dc3e4c39bc94ad6862b502bbb804a7d2de
Nelson Gill <RobotRogue@users.noreply.github.com>
2015-12-18 15:41:36 -0800

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,6 +1,8 @@
+#Original creators article found here: http://null-byte.wonderhowto.com/how-to/make-gmail-notifier-python-0132845/
 import sys, feedparser, winsound
-#More info here: http://null-byte.wonderhowto.com/how-to/make-gmail-notifier-python-0132845/
 
+
+#Assign variables with values. Fill in your username and password
 newEmail=""
 USERNAME="YOUR.GMAIL.ADDRESS@gmail.com"
 PASSWORD="YOUR PASSWORD"
@@ -8,26 +10,23 @@ PROTO="https://"
 SERVER="mail.google.com"
 PATH="/gmail/feed/atom"
 
-#We assign variables with values. Fill in your username and password
+#parses your account data and sends it to gmail
 def mail(checker):
     email = int(feedparser.parse(
         PROTO + USERNAME + ":" + PASSWORD + "@" + SERVER + PATH
     )["feed"]["fullcount"])
 
-#parses your account data and sends it to gmail
-    #checks for mail
+#checks for mail
     if email > 0:
         newEmail = 1
     else:
         newEmail = 0
 
-
-    #plays sound if email present
+#plays sound if email present
     if newEmail==1:
          winsound.Beep(440, 500)
          winsound.Beep(370, 500)
          winsound.Beep(392, 500)
-
 
 #need to replace winsound.Beep with piglow functionality
 #set the piglow functionality to pulse red when email is present (newEmail==1)
