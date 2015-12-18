969c059398c39a96ccdcd1f663aea449de94134e
Nelson Gill <RobotRogue@users.noreply.github.com>
2015-12-18 15:35:06 -0800

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,10 +1,11 @@
 import parser2, time
 
-#import modules
 x=1
 #set variable
+
 while True:
     parser2.mail(0)
     time.sleep(10)
+
 #re-run program to infinity, every ten seconds.
 #Change the "time.sleep(seconds to wait) variable if you want a longer/shorter check interval. 
